ae517020392df3b808343e08ff7d2485db8c641c
raghul1212 <raguldravid195@gmail.com>
2021-09-29 07:04:37 +0530

    idnotfound,duplicateid exceptions removed

--- src/main/java/com/revature/interviewmanagement/exception/IdNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.revature.interviewmanagement.exception;
-
-public class IdNotFoundException extends RuntimeException {
-	private static final long serialVersionUID = -4918980763600812322L;
-
-	public IdNotFoundException(String msg) {
-		super(msg);
-	}
-}
