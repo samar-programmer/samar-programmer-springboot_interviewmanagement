7bc204dde4581ce597f0f2812ccac43b580ecfb6
raghul1212 <raguldravid195@gmail.com>
2021-10-09 16:18:03 +0530

    changes added

--- src/main/java/com/revature/interviewmanagement/utils/MailSenderUtil.java
+++ src/main/java/com/revature/interviewmanagement/utils/MailSenderUtil.java
@@ -1,6 +1,5 @@
 package com.revature.interviewmanagement.utils;
 
-
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
@@ -10,40 +9,33 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
-
 public class MailSenderUtil {
-	private static final Logger logger=LogManager.getLogger(MailSenderUtil.class.getName());
-		private MailSenderUtil() {
-			throw new IllegalStateException("Utility class");
-		}
-		
-		private  static final int NO_OF_QUICK_SERVICE_THREADS = 20;
+	private static final Logger logger = LogManager.getLogger(MailSenderUtil.class.getName());
 
-		/**
-		 * this statement create a thread pool of twenty threads
-		 * here we are assigning send mail task using ScheduledExecutorService.submit();
-		 */
-		private static ScheduledExecutorService quickService = Executors.newScheduledThreadPool(NO_OF_QUICK_SERVICE_THREADS); // Creates a thread pool that reuses fixed number of threads(as specified by noOfThreads in this case).
-	   
-	    public static  void sendMail(JavaMailSender javaMailSender,String toReceiver,String subject,String message) throws MailException{		
-	    	logger.trace("entering sendMail method");
-			SimpleMailMessage msg = new SimpleMailMessage();
-    		msg.setTo(toReceiver);
-	        msg.setSubject(subject);
-	        msg.setText(message);
-	        quickService.submit(new Runnable() {
-				@Override
-				public void run() {
-					try{
-						javaMailSender.send(msg);
-					}catch(Exception e){
-						logger.error("Exception occur while send a mail : ",e);
-					}
-				}
-			});
-	       	
-			
-		}
-	
-	
+	private MailSenderUtil() {
+		throw new IllegalStateException("Utility class");
+	}
+
+	private static final int NO_OF_QUICK_SERVICE_THREADS = 20;
+
+	/**
+	 * this statement create a thread pool of twenty threads here we are assigning
+	 * send mail task using ScheduledExecutorService.submit();
+	 */
+	private static ScheduledExecutorService quickService = Executors
+			.newScheduledThreadPool(NO_OF_QUICK_SERVICE_THREADS); // Creates a thread pool that reuses fixed number of
+																	// threads(as specified by noOfThreads in this
+																	// case).
+
+	public static void sendMail(JavaMailSender javaMailSender, String toReceiver, String subject, String message)
+			throws MailException {
+		logger.trace("entering sendMail method");
+		SimpleMailMessage msg = new SimpleMailMessage();
+		msg.setTo(toReceiver);
+		msg.setSubject(subject);
+		msg.setText(message);
+		quickService.submit(() -> javaMailSender.send(msg));
+
+	}
+
 }
