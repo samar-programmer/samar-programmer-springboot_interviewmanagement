b0dc1c7f0a17940702f72a1f48131efdb87105c1
Pavithra Nagarajan <pavithranagarajan205@gmail.com>
2022-09-30 08:33:54 +0000

    file added

---
+++ src/test/java/com/revature/interviewmanagement/InterviewCopyFile.java
@@ -0,0 +1,13 @@
+package com.revature.interviewmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {//NOSONAR
+	}
+
+}
